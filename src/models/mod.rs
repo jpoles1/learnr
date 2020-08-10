a337bcb4c07865c0409c7612ab59903a8d10a305
Jordan Poles <jpdev.noreply@gmail.com>
2020-08-10 03:11:41 -0400

    Finishing DB ops

--- src/models/mod.rs
+++ src/models/mod.rs
@@ -19,11 +19,26 @@ impl LearnrEntry {
     pub async fn insert(&self) -> Result<(), String> {
         let collection = db::connect("entries".to_owned()).await;
         if let Document(doc) = to_bson(&self).unwrap() {
-            collection.insert_one(doc, None).await.unwrap();
+            let res = collection.insert_one(doc, None).await;
+            return match res {
+                Ok(_) => Ok(()),
+                Err(e) =>  Err(format!("Error inserting entry: {}", e))
+            }
         }
         return Err("Could not convert struct to doc".to_owned());
     }
-    
+    pub async fn update(&self) -> Result<(), String> {
+        let collection = db::connect("entries".to_owned()).await;
+        if let Document(doc) = to_bson(&self).unwrap() {
+            let query = doc!{"_id": self._id.clone()};
+            let res = collection.find_one_and_update(query, doc, None).await;
+            return match res {
+                Ok(_) => Ok(()),
+                Err(e) =>  Err(format!("Error updating entry: {}", e))
+            }
+        }
+        return Err("Could not convert struct to doc".to_owned());
+    }
     pub async fn delete(&self) -> Result<(), String> {
         let collection = db::connect("entries".to_owned()).await;
         let query = doc!{"_id": self._id.clone()};
